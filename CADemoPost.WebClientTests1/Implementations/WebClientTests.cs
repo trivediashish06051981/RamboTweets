838818c532eedb6b02fca023e735114f9712f87f
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-22 15:09:10 +0530

    Add files via upload

---
+++ CADemoPost.WebClientTests1/Implementations/WebClientTests.cs
@@ -0,0 +1,42 @@
+﻿using RamboTweets.DTO.Models;
+using RamboTweets.WebClient.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CADemoPost.WebClientTests1;
+using Microsoft.Practices.Unity;
+using System.Collections.Generic;
+using RamboTweets.Proxy.Interfaces;
+using RamboTweets.VM;
+
+namespace RamboTweets.WebClient.Implementations.Tests
+{
+    [TestClass()]
+    public class WebClientTests
+    {
+        [TestMethod()]
+        public void GetPostsTest()
+        {
+            IWebClient<Post> webClient = RamboContainer.GetContainer().Resolve<IWebClient<Post>>(
+                new ResolverOverride[]
+        {
+            new ParameterOverride("url", @"http://jsonplaceholder.typicode.com/posts")
+        });
+            var list = webClient.GetPosts();
+
+            Assert.IsNotNull(list);
+            Assert.IsInstanceOfType(list, typeof(IEnumerable<Post>));
+        }
+
+        public void GetPostsTestProxy()
+        {
+            IProxy<Post, PostViewModel> proxyClient = RamboContainer.GetContainer().Resolve<IProxy<Post, PostViewModel>>(
+                new ResolverOverride[]
+        {
+            new ParameterOverride("url", @"http://jsonplaceholder.typicode.com/posts")
+        });
+            var list = proxyClient.GetAll();
+
+            Assert.IsNotNull(list);
+            Assert.IsInstanceOfType(list, typeof(IEnumerable<PostViewModel>));
+        }
+    }
+}
---
+++ CADemoPost.WebClientTests1/RamboContainer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Unity;
+using RamboTweets.DTO.Models;
+using RamboTweets.Proxy;
+using RamboTweets.Proxy.Interfaces;
+using RamboTweets.VM;
+using RamboTweets.WebClient.Implementations;
+using RamboTweets.WebClient.Interfaces;
+
+namespace CADemoPost.WebClientTests1
+{
+    public static class RamboContainer
+    {
+        private static UnityContainer Container { get; set; }
+
+        public static UnityContainer GetContainer()
+        {
+            if(Container == null)
+            {
+                Container = new UnityContainer();
+                Container.RegisterType<IWebClient<Post>, WebClient<Post>>();
+                Container.RegisterType<IProxy<Post, PostViewModel>, Proxy<Post, PostViewModel>>();
+            }
+            return Container;
+        }
+    }
+}
