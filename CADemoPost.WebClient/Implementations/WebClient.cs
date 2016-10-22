13618f9c403cbd3a48e159209cee49c517347994
trivediashish06051981 <trivediashish.spamrs@gmail.com>
2016-10-22 15:08:32 +0530

    Add files via upload

---
+++ CADemoPost.WebClient/Implementations/WebClient.cs
@@ -0,0 +1,55 @@
+﻿using RamboTweets.WebClient.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RamboTweets.WebClient.Implementations
+{
+    /// <summary>
+    /// Class WebClient.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="RamboTweets.WebClient.Interfaces.IWebClient{T}" />
+    public class WebClient<T> : IWebClient<T> where T: class
+    {
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>The URL.</value>
+        public string Url { get; set; }
+        /// <summary>
+        /// Gets or sets the HTTP client.
+        /// </summary>
+        /// <value>The HTTP client.</value>
+        public HttpClient HttpClient { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebClient{T}"/> class.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        public WebClient(string url)
+        {
+            this.Url = url;
+            this.HttpClient = new HttpClient();
+            this.HttpClient.BaseAddress = new Uri(this.Url);
+        }
+
+        /// <summary>
+        /// Gets the posts.
+        /// </summary>
+        /// <returns>IEnumerable&lt;T&gt;.</returns>
+        public IEnumerable<T> GetPosts()
+        {
+            try
+            {
+                HttpResponseMessage response = HttpClient.GetAsync(Url).Result;
+                return response.Content.ReadAsAsync<IEnumerable<T>>().Result;
+            }
+            catch (AggregateException exception)
+            {
+                //Logging to Database if any
+                //Used catch if we have any network issues
+                return new List<T>();
+            }
+        } 
+    }
+}
---
+++ CADemoPost.WebClient/Interfaces/IWebClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RamboTweets.WebClient.Interfaces
+{
+    /// <summary>
+    /// Interface IWebClient
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IWebClient<T> where T: class
+    {
+        /// <summary>
+        /// Gets the posts.
+        /// </summary>
+        /// <returns>IEnumerable&lt;T&gt;.</returns>
+        IEnumerable<T> GetPosts();
+    }
+}
